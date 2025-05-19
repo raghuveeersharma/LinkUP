688ef627b039e2513c2c8340a790768525319577
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-05-19 06:44:53 +0530

    create generateStreamToken func

--- server/src/lib/stream.js
+++ server/src/lib/stream.js
@@ -18,3 +18,12 @@ export const upsertStreamUser = async (userData) => {
     console.log("error in upsertUser", err);
   }
 };
+export const generateStreamToken = (userId) => {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream token:", error);
+  }
+};
