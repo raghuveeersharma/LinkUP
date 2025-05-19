664fc65805f70fd16ef92f695f529d12d421acb7
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-05-19 06:46:29 +0530

    use chat route

--- server/src/sever.js
+++ server/src/sever.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRouter);
+app.use("/api/chat", chatRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
