d78bbdc6408b5b8deb78d618d3fcc8f57700ba6d
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-05-29 13:02:49 +0530

    add dropdown menu to mobile screen

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
-import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
+import { BellIcon, LogOutIcon, Menu, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [MenuOpen, setMenuOpen] = useState(false);
   const { authUser } = useAuthUser();
   const location = useLocation();
   const isChatPage = location.pathname?.startsWith("/chat");
 
   const { logoutMutation } = useLogout(); // custom hook for logout
+  const toggleMobileMenu = () => {
+    setMenuOpen(!MenuOpen);
+  };
 
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
@@ -21,13 +26,21 @@ const Navbar = () => {
               <Link to="/" className="flex items-center gap-2.5">
                 <ShipWheelIcon className="size-9 text-primary" />
                 <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                  Streamify
+                  LinkUP
                 </span>
               </Link>
             </div>
           )}
+          <div className="lg:hidden fixed left-4">
+            <Link to="/" className="flex items-center gap-1.5">
+              <ShipWheelIcon className="size-9 text-primary" />
+              <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+                LinkUP
+              </span>
+            </Link>
+          </div>
 
-          <div className="flex items-center ml-auto">
+          <div className="sm:flex hidden items-center ml-auto">
             <Link to={"/notifications"}>
               <button className="btn btn-ghost btn-circle">
                 <BellIcon className="h-6 w-6 text-base-content opacity-70" />
@@ -38,7 +51,7 @@ const Navbar = () => {
           {/* TODO */}
           <ThemeSelector />
 
-          <div className="avatar">
+          <div className="avatar hidden sm:block">
             <Link to={`/profile/${authUser?._id}`}>
               <div className="w-9 rounded-full">
                 <img
@@ -51,10 +64,57 @@ const Navbar = () => {
           </div>
 
           {/* Logout button */}
-          <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
+          <button
+            className="btn btn-ghost btn-circle hidden sm:block"
+            onClick={logoutMutation}
+          >
             <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
           </button>
         </div>
+      </div>
+      {/* moobile menu */}
+      <div
+        className="dropdown dropdown-end sm:hidden"
+        onClick={toggleMobileMenu}
+      >
+        <label tabIndex={0} className="btn btn-ghost btn-circle">
+          <Menu className="h-6 w-6 text-base-content opacity-70" />
+        </label>
+        {MenuOpen && (
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/" className="flex items-center gap-2">
+                <ShipWheelIcon className="h-6 w-6 text-base-content opacity-70" />
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={"/notifications"}>
+                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
+                Notifications
+              </Link>
+            </li>
+            <li>
+              <Link to={`/profile/${authUser?._id}`}>
+                <img
+                  src={authUser?.profilePic}
+                  alt="User Avatar"
+                  className="w-6 h-6 rounded-full"
+                />
+                Profile
+              </Link>
+            </li>
+            <li>
+              <button onClick={logoutMutation}>
+                <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
+                Logout
+              </button>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
